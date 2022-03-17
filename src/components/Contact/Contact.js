79d41414bc9920f7d96a840e821a7c0201a78760
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2022-03-17 00:35:36 -0700

    Created contact form and added emailJs library to recieve email

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,48 @@
-import React from 'react'
-import './Contact.scss'
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
+import "./Contact.scss";
 
 export default function Contact() {
+  const form = useRef();
+  const [ emailIsSent, setEmailIsSent ] = useState(false)
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    emailjs
+      .sendForm(
+        "service_n58a7ol",
+        "template_2ktlgrn",
+        form.current,
+        "gwgBQq0LJ6h3tVkpq"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setEmailIsSent(true)
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
-    <div className='contact-me' id='contact-me'>
+    <div className="contact-me" id="contact-me">
+      <div className="left"></div>
+      <div className="contact-container">
+        <h2>Contact</h2>
+        <form ref={form} onSubmit={handleSubmit}>
+          <input type="text" placeholder="Name" name="sender_name"></input>
+          <input
+            type="text"
+            placeholder="Subject"
+            name="sender_subject"
+          ></input>
+          <input type="text" placeholder="Email" name="sender_email"></input>
+          <textarea rows="5" placeholder="Message" name="message"></textarea>
+          <button type="submit">Send</button>
+          <span>{emailIsSent ? 'Thank You, I will reply ASAP': ''}</span>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
