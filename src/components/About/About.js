9b2285e793d853c50fca394853df837cf1b359f8
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2022-03-20 02:50:53 -0700

    Made the About page responsive

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,39 +1,45 @@
-import React, { useEffect, useRef } from 'react';
-import { init } from 'ityped'
-import {KeyboardArrowDown} from '@material-ui/icons'
-import './About.scss'
+import React, { useEffect, useRef } from "react";
+import { init } from "ityped";
+import { KeyboardArrowDown } from "@material-ui/icons";
+import "./About.scss";
 
 export default function About() {
-  const roleRef = useRef()
+  const roleRef = useRef();
 
   useEffect(() => {
+    init(roleRef.current, {
+      showCursor: false,
+      strings: ["Full Stack Developer"],
+    });
+  }, []);
 
-    init(roleRef.current, 
-      { 
-        showCursor: false, 
-        strings: ['Full Stack Developer' ] 
-      })
-
-  }, [])
-
-  return (  
-    <div className='about-me' id='about-me'>
-      <div className='left-side'>
-        <div className='photo-container'>
-          <img src='assets/myPicture.png' id='photo'></img>
+  return (
+    <div className="about-me" id="about-me">
+      <div className="left-side">
+        <div className="photo-container">
+          <img src="assets/myPicture.png" id="photo"></img>
         </div>
       </div>
 
-      <div className='right-side'>
-        <div className='right-side-wrapper'>
+      <div className="right-side">
+        <div className="right-side-wrapper">
           <h2>Hi, I am</h2>
           <h1>Hekmatullah Salehi</h1>
-          <h3>Aspiring <span ref={roleRef}></span></h3>
+          <h3>
+            Aspiring <span ref={roleRef}></span>
+          </h3>
+          <p>
+            Acquired skills in HTML, CSS, JavaScript, Node.js, Express.js,
+            React, MySQL, MongoDB, and Git Workflow. Problem-solving ability,
+            fast learner, detail-oriented with the ability to work in stressful
+            situations. Passionate to develop real-world projects from
+            conception.
+          </p>
         </div>
         <a href="#projects">
-          <KeyboardArrowDown className='arrow-down-icon'/>
+          <KeyboardArrowDown className="arrow-down-icon" />
         </a>
       </div>
     </div>
-  )
+  );
 }
