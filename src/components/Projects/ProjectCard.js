f2654c4e6f2fe3df1a0ed12b71e53449a064943a
Hekmatullah Salehi <hekmatullahsalehi@gmail.com>
2022-04-01 01:36:15 -0700

    Removed the overflow for mobile view

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -5,7 +5,7 @@ import { Launch, GitHub } from "@material-ui/icons";
 export default function Projects(props) {
   return (
     <div className="projects" id="projects">
-      <h1>Projects</h1>
+      <h2>Projects</h2>
       <div className="projects-container">
         {props.projects.map((project) => {
           return (
